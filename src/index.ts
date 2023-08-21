3c1115cc0a2ca097d51ff14479396a3b293d4e2c
CiceroLucas <lukasferreiradesousa890@gmailcom>
2023-08-20 21:49:58 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express from "express";
+import { config } from "dotenv";
 
+config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => console.log(`server running on por ${port}`));
